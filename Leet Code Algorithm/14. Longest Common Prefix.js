78d3caad476c8eb5eb1a477fe73905ae96704547
Lucas Augusto <lucasaugustoleon@gmail.com>
2023-03-13 22:55:22 -0300

    1 algorithm

--- Leet Code Algorithm/14. Longest Common Prefix.js
+++ Leet Code Algorithm/14. Longest Common Prefix.js
@@ -2,7 +2,7 @@ const LongestCommonPrefix = (strs) => {
     if(!strs.length) return "";
     for(let i = 0; i < strs[0].length; i++) {
         for (let str of strs) {
-            if(str[0] !== strs[0][i]){
+            if(str[i] !== strs[0][i]){
                 return str.slice(0, i)
             }
         }
