ad7dcf7947f7aeb475ac909b2a104f14306f2ce5
Lucas Augusto <lucasaugustoleon@gmail.com>
2023-03-17 10:18:17 -0300

    1 algorithm

---
+++ Leet Code Algorithm/1. Two Sum.js
@@ -0,0 +1,10 @@
+const twoSum = (nums, target) => {
+    let map = new Map();
+    for(let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if(map.has(complement)) {
+            return[i, map.get(complement)];
+        }
+        map.set(nums[i], i);
+    }
+};
